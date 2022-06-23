904f6607f2ab21fc076e256f3cd3761fae002057
Suenggoo.Lee <akajqtk96@naver.com>
2022-06-23 12:05:22 +0900

    Django3 member join views, template 수정 - 클라이언트측 /서버측 유효성 검사,Meber 객체 생성후 디비 저장, 가입완료시 ok페이지로 전환

--- member/urls.py
+++ member/urls.py
@@ -2,7 +2,8 @@ from django.urls import  path
 from . import  views
 
 urlpatterns=[
-    path('join/',views.join , name='join'),
-    path('login/',views.login , name='login'),
-    path('myinfo/',views.myinfo , name='myinfo'),
+    # 127.0.0.1:8000/join 요청시 member apps의 join함수가 처리함
+    path('join/', views.join , name='join'),
+    path('login/', views.login , name='login'),
+    path('myinfo/', views.myinfo , name='myinfo'),
 ]
--- member/views.py
+++ member/views.py
@@ -1,8 +1,45 @@
+from django.contrib.auth.hashers import make_password
 from django.shortcuts import render
+from member.models import Member
 
 # Create your views here.
 def join(request):
-    return  render(request,'member/join.html')
+    returnPage='member/join.html'
+    if request.method == 'GET':
+        return render(request,'member/join.html')
+    elif request.method=='POST':
+        #폼으로 전송된 데이터들을 dict 형태로 저장
+        form =request.POST.dict()
+        # print(form,form['userid'])#전송된 데이터 확인
+
+        # 유효성 검사 1/2만 3/4/5/
+        error = ''  # 검사 결과 저장용 변수
+        if not(form['userid'] and form['passwd'] and form['repasswd'] and form['name'] and form['email']):
+            error = '입력값이 누락되었습니다.'
+        elif (form['passwd'] != form['repasswd']) :
+            error = '비밀번호가 일치하지 않습니다.!'
+        else:
+            #입력한 회원 정보를 Member 객체에 담음
+
+
+            member= Member(userid=form['userid'],
+                           passwd=make_password(form['passwd']),# 비밀번호는 암호화
+                           name=form['name'],email=['email']
+            )
+
+            #Member객체에 담은 회원정보를 member 테이블에 저장
+            member.save()
+
+            #회원 가입 성공시 joinok.html를 뛰움
+            returnPage='member/joinok.html'
+
+        #유효성 검사를 실패하는 경우 오류내용을 join.html에 표시하기 위해 dict변수에 저장
+        #또한 , 이미 입력했던 회원정보를 다시 join.html에 표시하기위해
+        #form이라는 dict변수 생성
+
+        context = {'form':form,'error':error}
+
+        return  render(request,returnPage,context)
 
 
 def login(request):
