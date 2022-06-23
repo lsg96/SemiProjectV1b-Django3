1350f52465b721a805a5928dbb77d0c0b4d9bb0d
Suenggoo.Lee <akajqtk96@naver.com>
2022-06-23 12:50:22 +0900

    Django3 member join/joinok 수정

--- SemiProjectV1b_Django3/settings.py
+++ SemiProjectV1b_Django3/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
     'board.apps.BoardConfig',
-    'member.apps.MemberConfig',
+    'member.apps.MemberConfig'
 ]
 
 MIDDLEWARE = [
