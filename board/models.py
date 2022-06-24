dce0d50451c98dabb43332eac5c9aba5f1d24b20
Suenggoo.Lee <akajqtk96@naver.com>
2022-06-24 15:20:44 +0900

    Django3 board 새글쓰기 리스트 출력

--- board/models.py
+++ board/models.py
@@ -1,3 +1,26 @@
+from datetime import datetime
+from member.models import Member
 from django.db import models
 
 # Create your models here.
+# on_delete: CASCADE, DO_NOTHING
+
+class board(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    userid = models.ForeignKey(Member, db_column='userid', on_delete=models.CASCADE)
+
+    regdate = models.DateTimeField(default=datetime.now)
+    views = models.IntegerField(default=0)
+    contents = models.TextField(null=False, blank=False)
+
+    class Meta:
+        db_table = 'board'
+        ordering = ['-regdate']
+
+    def __str__(self):
+        return  self.title
+
+
+
+
--- board/views.py
+++ board/views.py
@@ -1,13 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from member.models import Member
 
 # Create your views here.
+from board.models import board
+
+
 def list(request):
-    return render(request, 'board/list.html')
+#게시판 목록 보기
+    bdlist = board.objects.values('id','title','userid','regdate','views')\
+    .order_by('-id')
 
+    context={'bds':bdlist}
+    return render(request, 'board/list.html',context)
 
+#게시판 본문 보기
 def view(request):
     return render(request, 'board/view.html')
 
 
+#게시판 글쓰기
+#get : board/write.html
+#post:작성한 글을 디비에 저장, board/list.html로 이동
 def write(request):
-    return render(request, 'board/write.html')
+    returnPage = 'board/write.html'
+    error = ''
+    form = ''
+    if request.method =='GET':
+        pass
+    elif request.method == 'POST':
+        form=request.POST.dict()
+
+        #유효성 검사1
+
+        if not (form['title'] and form['contents']):
+            error='제목이나 본문을 작성하세요!'
+        else:
+            # 입력한 게시글을 Board 객체에 담음
+
+            bd=board(title=form['title'],
+                     contents=form['contents'],
+                     #새글을 작성한 작성한 회원에 대한 정보는
+                     #회원테이블에 존재하는 회원아이디를 조회해서
+                     #userid속성에 저장
+                     userid=Member.objects.get(pk=form['memberid'])
+                     )
+            bd.save() #Board객체에 담은 게시글을 테이블에 저장
+            return  redirect('/list')
+    context = {'form': form, 'error': error}
+
+    return render(request, returnPage,context)
+
